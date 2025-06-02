ddd3c8c440b5bb903d89c9b9e35e7e3e60404fe2
Timofei Maksimov <maxtimofy@gmail.com>
2025-06-02 18:39:11 +0200

    fix: remove email delivery

--- lib/my_podcasts_web/live/auth/user_settings_live.ex
+++ lib/my_podcasts_web/live/auth/user_settings_live.ex
@@ -13,28 +13,6 @@ defmodule MyPodcastsWeb.UserSettingsLive do
     <div class="space-y-12 divide-y">
       <div>
         <.simple_form
-          for={@email_form}
-          id="email_form"
-          phx-submit="update_email"
-          phx-change="validate_email"
-        >
-          <.input field={@email_form[:email]} type="email" label="Email" required />
-          <.input
-            field={@email_form[:current_password]}
-            name="current_password"
-            id="current_password_for_email"
-            type="password"
-            label="Current password"
-            value={@email_form_current_password}
-            required
-          />
-          <:actions>
-            <.button phx-disable-with="Changing...">Change Email</.button>
-          </:actions>
-        </.simple_form>
-      </div>
-      <div>
-        <.simple_form
           for={@password_form}
           id="password_form"
           action={~p"/users/log_in?_action=password_updated"}
@@ -101,38 +79,6 @@ defmodule MyPodcastsWeb.UserSettingsLive do
       |> assign(:trigger_submit, false)
 
     {:ok, socket}
-  end
-
-  def handle_event("validate_email", params, socket) do
-    %{"current_password" => password, "user" => user_params} = params
-
-    email_form =
-      socket.assigns.current_user
-      |> Accounts.change_user_email(user_params)
-      |> Map.put(:action, :validate)
-      |> to_form()
-
-    {:noreply, assign(socket, email_form: email_form, email_form_current_password: password)}
-  end
-
-  def handle_event("update_email", params, socket) do
-    %{"current_password" => password, "user" => user_params} = params
-    user = socket.assigns.current_user
-
-    case Accounts.apply_user_email(user, password, user_params) do
-      {:ok, applied_user} ->
-        Accounts.deliver_user_update_email_instructions(
-          applied_user,
-          user.email,
-          &url(~p"/users/settings/confirm_email/#{&1}")
-        )
-
-        info = "A link to confirm your email change has been sent to the new address."
-        {:noreply, socket |> put_flash(:info, info) |> assign(email_form_current_password: nil)}
-
-      {:error, changeset} ->
-        {:noreply, assign(socket, :email_form, to_form(Map.put(changeset, :action, :insert)))}
-    end
   end
 
   def handle_event("validate_password", params, socket) do
--- lib/my_podcasts_web/router.ex
+++ lib/my_podcasts_web/router.ex
@@ -4,23 +4,18 @@ defmodule MyPodcastsWeb.Router do
   import MyPodcastsWeb.UserAuth
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_live_flash
-    plug :put_root_layout, html: {MyPodcastsWeb.Layouts, :root}
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-    plug :fetch_current_user
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_live_flash)
+    plug(:put_root_layout, html: {MyPodcastsWeb.Layouts, :root})
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
+    plug(:fetch_current_user)
   end
 
   pipeline :api do
-    plug :accepts, ["xml"]
+    plug(:accepts, ["xml"])
   end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", MyPodcastsWeb do
-  #   pipe_through :api
-  # end
 
   # Enable LiveDashboard and Swoosh mailbox preview in development
   if Application.compile_env(:my_podcasts, :dev_routes) do
@@ -32,60 +27,50 @@ defmodule MyPodcastsWeb.Router do
     import Phoenix.LiveDashboard.Router
 
     scope "/dev" do
-      pipe_through :browser
+      pipe_through(:browser)
 
-      live_dashboard "/dashboard", metrics: MyPodcastsWeb.Telemetry
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
+      live_dashboard("/dashboard", metrics: MyPodcastsWeb.Telemetry)
+      forward("/mailbox", Plug.Swoosh.MailboxPreview)
     end
   end
 
   scope "/", MyPodcastsWeb do
-    pipe_through :api
+    pipe_through(:api)
 
-    get "/rss_feed/:feed_hash", RssController, :index
-    get "/file/:file_hash", FileController, :index
+    get("/rss_feed/:feed_hash", RssController, :index)
+    get("/file/:file_hash", FileController, :index)
   end
 
   ## Authentication routes
-#
   scope "/", MyPodcastsWeb do
-    pipe_through [:browser, :redirect_if_user_is_authenticated]
+    pipe_through([:browser, :redirect_if_user_is_authenticated])
 
     live_session :redirect_if_user_is_authenticated,
       on_mount: [{MyPodcastsWeb.UserAuth, :redirect_if_user_is_authenticated}] do
-      live "/users/register", UserRegistrationLive, :new
-      live "/users/log_in", UserLoginLive, :new
-      live "/users/reset_password", UserForgotPasswordLive, :new
-      live "/users/reset_password/:token", UserResetPasswordLive, :edit
+      live("/users/register", UserRegistrationLive, :new)
+      live("/users/log_in", UserLoginLive, :new)
     end
 
-    post "/users/log_in", UserSessionController, :create
+    post("/users/log_in", UserSessionController, :create)
   end
 
   scope "/", MyPodcastsWeb do
-    pipe_through [:browser, :require_authenticated_user]
+    pipe_through([:browser, :require_authenticated_user])
 
     live_session :require_authenticated_user,
       on_mount: [{MyPodcastsWeb.UserAuth, :ensure_authenticated}] do
-      live "/", New
+      live("/", New)
 
-      live "/files", FileLive.Index, :index
-      live "/files/:id/edit", FileLive.Index, :edit
+      live("/files", FileLive.Index, :index)
+      live("/files/:id/edit", FileLive.Index, :edit)
 
-      live "/users/settings", UserSettingsLive, :edit
-      live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
+      live("/users/settings", UserSettingsLive, :edit)
     end
   end
 
   scope "/", MyPodcastsWeb do
-    pipe_through [:browser]
+    pipe_through([:browser])
 
-    delete "/users/log_out", UserSessionController, :delete
-
-    live_session :current_user,
-      on_mount: [{MyPodcastsWeb.UserAuth, :mount_current_user}] do
-      live "/users/confirm/:token", UserConfirmationLive, :edit
-      live "/users/confirm", UserConfirmationInstructionsLive, :new
-    end
+    delete("/users/log_out", UserSessionController, :delete)
   end
 end
